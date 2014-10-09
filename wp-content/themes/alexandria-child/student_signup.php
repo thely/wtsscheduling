ca7b96d25fccdcbba7e4dbf7f509c6434dc0bfb8
thely <rbrown4@mail.umw.edu>
2014-10-09 11:21:35 -0400

    dummy calendar?

--- wp-content/themes/alexandria-child/student_signup.php
+++ wp-content/themes/alexandria-child/student_signup.php
@@ -17,8 +17,32 @@
 	<script type="text/javascript" src="/js/lib/moment.min.js"></script>
 	<script type="text/javascript" src="/js/fullcalendar.min.js"></script>
 	<script type="text/javascript" src="/js/gcal.js"></script>
-	
+	<script>
+		$(document).ready(function() {
+			$('#calendar').fullCalendar({
+				events: { url 'https://www.google.com/calendar/feeds/engima.in.my.soup%40gmail.com/public/basic' },
+				eventClick: function() {
+					return false;
+				}
+				header: {
+					left: "prev,next today",
+					center: "title",
+					right: "agendaWeek,month"
+				}
+			});
+		});
+	</script>
+	<style>
+		#calendar {
+			max-width: 900px;
+			margin: 0 auto;
+		}
+	</style>
 </head>
+<body>
+	<div id='calendar'></div>
+</body>
+
 <?php
 
 	get_footer(); 
